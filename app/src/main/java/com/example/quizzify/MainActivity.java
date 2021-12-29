3f3db0abaeec49837d1b4525a179085d53a4ba5b
Kaustubh Solanki <kaustubhsolanki@Kaustubhs-MacBook-Air.local>
2021-12-30 00:46:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quizzify/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.quizzify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            this.getSupportActionBar().hide();
+        }catch (NullPointerException e){}
+        setContentView(R.layout.activity_main);
+    }
+}
